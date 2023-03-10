ee223f05c425626a0805145bb78cec32f16e7305
unknown <ricardo.ryt@gmail.com>
2023-03-10 20:15:44 -0300

    inicializacao com state

--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      latitude: null,
+  state = {
+    latitude: null,
       longitude: null,
       estacao: null,
       data: null,
-      icone: null
-    }
-    console.log('constructor')
+      icone: null,
+      mensagemDeErro: null
   }
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     latitude: null,
+  //     longitude: null,
+  //     estacao: null,
+  //     data: null,
+  //     icone: null
+  //   }
+  //   console.log('constructor')
+  // }
 
   componentDidMount(){
     this.obterLocalizacao()
